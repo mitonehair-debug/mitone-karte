f830b1d06d0c6a056711c23b2cbf4d5ca22dba00
mitonehair-debug <mitone.hair@gmail.com>
2026-04-11 20:40:48 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Checkin from "./Checkin.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const path = window.location.pathname;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {path === "/checkin" ? <Checkin /> : <App />}
   </React.StrictMode>
-)
-
+);
